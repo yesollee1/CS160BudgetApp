e77a5ac1d5dd88cb02a941546fca6c6847180fa1
JaimeAdele <jaime.zuspann@gmail.com>
2022-03-23 18:30:30 -0700

    Adding Category class

---
+++ app/src/main/java/cs160/dataLayer/Category.java
@@ -0,0 +1,25 @@
+package cs160.dataLayer;
+
+public abstract class Category {
+    private String mName;
+    private Double mProposedAmount;
+    private Double mCurrentAmount;
+
+    public Category(String name, Double proposedAmount, Double currentAmount) {
+        mName = name;
+        mProposedAmount = proposedAmount;
+        mCurrentAmount = currentAmount;
+    }
+
+    public Double getPercent() {
+        return mCurrentAmount / mProposedAmount;
+    }
+
+    public void edit() {
+
+    }
+
+    public void delete() {
+        
+    }
+}
