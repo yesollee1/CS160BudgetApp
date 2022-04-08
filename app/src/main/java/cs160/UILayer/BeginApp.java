026d510df2a7b8bd4e45bd471dccdfa3b383d7e0
Ibrahima Wone <ibrahima.wone@sjsu.edu>
2022-04-08 00:10:53 -0700

    Google Sign In Implemented

---
+++ app/src/main/java/cs160/UILayer/BeginApp.java
@@ -0,0 +1,14 @@
+package cs160.UILayer;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class BeginApp extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_begin_app);
+    }
+}
