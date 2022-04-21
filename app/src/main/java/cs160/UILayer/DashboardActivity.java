05a6f7925f85fa0d8b88eb7fc7a221143ba1a005
Ibrahima Wone <33718398+Woneibra13@users.noreply.github.com>
2022-04-20 19:56:02 -0700

    Google Sign with Firebase. Dashboard Activity used as home.

--- app/src/main/java/cs160/UILayer/DashboardActivity.java
+++ app/src/main/java/cs160/UILayer/DashboardActivity.java
@@ -6,20 +6,47 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
+
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 
 public class DashboardActivity extends AppCompatActivity {
+
+    private FirebaseAuth firebaseAuth;
+
     private Button goalList;
     private Button expenseList;
     private Button transactionList;
+    private Button logoutBtn;
+    private TextView signedInUser;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_dummy);
 
+        //init firebaseAuth
+        firebaseAuth = FirebaseAuth.getInstance();
+        //checkUser();
+
+
+
+
+
         goalList = findViewById(R.id.linkToGoal);
         expenseList = findViewById(R.id.linkToExpense);
         transactionList = findViewById(R.id.linkToTransaction);
+        logoutBtn = findViewById(R.id.logoutBtn);
+        signedInUser = findViewById(R.id.signedInUser);
+
+        logoutBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                firebaseAuth.signOut();
+                checkUser();
+            }
+        });
 
         goalList.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -46,5 +73,22 @@ public class DashboardActivity extends AppCompatActivity {
         });
 
 
+    }
+
+    private void checkUser() {
+        //get current user
+        FirebaseUser firebaseUser = firebaseAuth.getCurrentUser();
+        if(firebaseUser == null){
+            // user not logged in
+            startActivity(new Intent(this, SignIn_Activity.class));
+            finish();
+        }
+        else{
+            //user logged in
+            // get user info
+            String email = firebaseUser.getEmail();
+            //set email
+            signedInUser.setText(email);
+        }
     }
 }
