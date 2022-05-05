fc692cf4c987338b5c24b08a87377ea111f41a9e
mrcyrilgoud <mrcyrilgoud@gmail.com>
2022-05-05 15:13:36 -0700

    Made addTransactions work

--- app/src/main/java/cs160/dataLayer/DatabaseManager.java
+++ app/src/main/java/cs160/dataLayer/DatabaseManager.java
@@ -20,6 +20,26 @@ public class DatabaseManager {
                 document(category.getTitle()).set(category,SetOptions.merge());
     }
 
+    public void addToGoals(Category category) {
+        FirebaseUser current = mAuth.getCurrentUser();
+        db.collection("Users").document(current.getUid()).collection("Goals").
+                document(category.getTitle()).set(category,SetOptions.merge());
+    }
+
+    public void addToTransactions(Transaction receipt) {
+        FirebaseUser current = mAuth.getCurrentUser();
+
+        /**Map<String, Object> transact = new HashMap<>();
+        transact.put("Title", receipt.getMerchant());
+        transact.put("Amount", receipt.getAmount());
+        transact.put("Date", receipt.getDate());
+        transact.put("Merchant", receipt.getMerchant());
+        transact.put("Notes", receipt.getNotes());**/
+
+        db.collection("Users").document(current.getUid()).collection("Transactions").
+                document(String.valueOf(receipt.getId())).set(receipt,SetOptions.merge());
+    }
+
     public static void addNewUser(String uid, String name) {
         final FirebaseFirestore db = FirebaseFirestore.getInstance();
         Map<String, Object> data = new HashMap<>();
